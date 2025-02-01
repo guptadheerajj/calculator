255da53246838274286aadbfffd5e363befc9039
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-02-01 14:06:12 +0530

    fix: more than one decimal in the number
    - allowed only one decimal to be entered in the number
    - previously more than one decimals were allowed to enter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Functionalities to be fixed: 
+// Functionalities to be fixed:
 // 1. Rounding large numbers to specific decimals
 // 2. Ensure multiple decimal points are not entered by the user
 
@@ -23,7 +23,7 @@ function multiply(operand1, operand2) {
 	return operand1 * operand2;
 }
 function divide(operand1, operand2) {
-	return operand2 === 0 ? "Nice Try Giga Diddy" : operand1 / operand2;
+	return operand2 === 0 ? "Nice Try Giga Diddy" : operand1  / operand2;
 }
 function remainder(operand1, operand2) {
 	return operand1 % operand2;
@@ -55,12 +55,24 @@ function operate(operand1, operator, operand2) {
 }
 
 function display(event) {
+	// what ever num buttons i press this is the condition that gets executed as it checks for displayValue === null, so solve . issue to make it 0. i need to modify this statement
 	// this to ensure that 0 is not present at the front of number. ex: 0890, 00090 instead it becomes 890 and 90
 	if (displayValue === "0" || displayValue === "00" || displayValue === null) {
-		displayValue = event.target.value;
+		console.log("before", displayValue);
+		if (event.target.value === ".") {
+			displayValue = "0";
+			displayValue += event.target.value;
+		} else {
+			displayValue = event.target.value;
+		}
+		console.log("After", displayValue);
 	} else if (displayValue.length >= 14) {
-		//number pressed after the display value reacehs >= 14 are not considered
+		//number pressed after the display value reaches >= 14 are not considered
 		displayValue = displayValue;
+	} else if (event.target.value === ".") {
+		if (!displayValue.includes(".")) {
+			displayValue += event.target.value;
+		}
 	} else {
 		displayValue += event.target.value;
 	}
@@ -82,6 +94,7 @@ operatorBtns.forEach((operatorBtn) => {
 			displayValue = operate(operand1, operator, operand2);
 			output.textContent = displayValue;
 
+			// stores the value of operator pressed after entering op1 and op2 and displaying their result
 			operator = event.target.value;
 			operand1 = displayValue;
 			operand2 = null;
@@ -89,8 +102,8 @@ operatorBtns.forEach((operatorBtn) => {
 		}
 
 		// this ensures if an operator is pressed multiple times after entering operand1 and at the end a different operator is pressed,
-		// 		the the last pressed operator will be cosidered.
-		// This also stores the value of operator pressed when both operands have been received and result is caluculated
+		// 		the the last pressed operator will be considered.
+		// This also stores the value of operator pressed when both operands have been received and result is calculated
 		// 		and stored in operand1 and this operator is stored as the operator for next calculation
 		if (operand2 === null) {
 			operator = event.target.value;
